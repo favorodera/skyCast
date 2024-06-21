36b69dd0bb1ac4bc10446d9253dc81ad27118553
Favour Chidera Emeka <favorodera@gmail.com>
2024-06-21 09:20:27 +0100

    update

---
+++ stores/weather-data.ts
@@ -0,0 +1,47 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { ref, computed } from 'vue'
+
+interface CurrentWeatherData {
+  current: {
+    cloud: string
+  }
+  location: {
+    localtime: string
+  }
+}
+
+export const useWeatherDataStore = defineStore('weather-data', () => {
+  const apiCurrentUrl = ref('https://api.weatherapi.com/v1/current.json')
+  const apiForecastUrl = ref('http://api.weatherapi.com/v1/forecast.json')
+  const currentWeatherData = ref<CurrentWeatherData | null>(null)
+  const apiKey = ref(import.meta.env.VITE_WEATHER_API_KEY)
+
+  const localtime = computed(() => {
+    return currentWeatherData.value?.location?.localtime.slice(10) || ''
+  })
+  const cloud = computed(() => {
+    return currentWeatherData.value?.current?.cloud || ''
+  })
+
+  const fetchWeatherData = async (): Promise<CurrentWeatherData | null> => {
+    try {
+      const response = await axios.get(`${apiCurrentUrl.value}?key=${apiKey.value}&q=canada`)
+      const data: CurrentWeatherData = response.data
+      currentWeatherData.value = data
+      console.log(data)
+
+      return data
+    } catch (error) {
+      console.error('Error fetching weather data:', error)
+      return null
+    }
+  }
+
+  return {
+    currentWeatherData: computed(() => currentWeatherData.value),
+    fetchWeatherData,
+    localtime,
+    cloud
+  }
+})
--- vue/components/BriefDailyOverview.vue
+++ vue/components/BriefDailyOverview.vue
@@ -1,7 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useWeatherDataStore } from '../../stores/weather-data'
+const weatherDataStore = useWeatherDataStore()
+onMounted(() => {
+  weatherDataStore.fetchWeatherData()
+})
+</script>
+
 <template>
   <div class="brief-daily-overview-container">
-    <div class="date-and-time"></div>
-    <div class="temperature-and-weather-icon-container"></div>
+    <div class="date-and-time">
+      <p>Friday</p>
+      <p>{{ weatherDataStore.localtime }} AM</p>
+    </div>
+    <div class="temperature-and-weather-icon-container">
+      <p>{{ weatherDataStore.cloud }}</p>
+    </div>
     <div class="brief-daily-details"></div>
   </div>
 </template>
@@ -14,5 +28,21 @@
   max-width: 15.0625rem;
   height: 14.125rem;
   border-radius: 1.5625rem;
+  align-items: center;
+}
+
+.date-and-time {
+  max-height: 3.5rem;
+  max-width: 100%;
+  padding: 1.13rem 0.88rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 1.5625rem 1.5625rem 0rem 0rem;
+  border-bottom: 0.0625rem solid #39393a;
+
+  p {
+    @include index.typography(1rem, 600, #e5e5e5, normal);
+  }
 }
 </style>
